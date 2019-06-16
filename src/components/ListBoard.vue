bfd3e8795e2d33ef0e4cf7bf0434349af75a7e02
Yasaka-H <yasaka245.ac@gmail.com>
2019-06-16 13:46:20 +0900

    add client side files

---
+++ src/components/ListBoard.vue
@@ -0,0 +1,76 @@
+<template>
+    <div>
+    <!---
+    <div class="showList" v-for="item in inOutList">
+
+    </div>
+    <span v-for="item in inOutList" v-bind:key="item.id">
+        <p>{{ item.id }} {{ item.date }} {{ item.category }} {{ item.item }} {{ item.price }} {{ item.discription }}</p>
+    </span> --->
+        <template>
+            <el-table
+                    :data="inOutList"
+                    style="width: 100%">
+                <el-table-column
+                        prop="date"
+                        label="Date"
+                        width="180"></el-table-column>
+                <el-table-column
+                        prop="category"
+                        label="Category"
+                        width="180"></el-table-column>
+                <el-table-column
+                        prop="item"
+                        label="Item"
+                        width="180"></el-table-column>
+                <el-table-column
+                    prop="price"
+                    label="Price"
+                    width="180"></el-table-column>
+                <el-table-column
+                    prop="discription"
+                    label="Discription"
+                    width="180"></el-table-column>
+            </el-table>
+        </template>
+    <AddList />
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+
+    import AddList from './parts/AddList.vue'
+    export default {
+        name: "ListBoard",
+        data() {
+            return {
+                inOutList: [],
+                loading: true,
+                errored: false,
+            }
+        },
+        components:{
+            AddList
+        },
+        mounted() {
+            axios.get('http://localhost:4567/get_all')
+                .then(
+                    response=>{
+                        this.inOutList = response.data
+                        // this.data.inOutList = JSON.parse(this.getList)
+                        // console.log(this.getList)
+                    }
+                ).catch(
+                    error =>{
+                        alert(error)
+                        this.errored = true
+                    }
+            )
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/components/parts/AddList.vue
@@ -0,0 +1,73 @@
+<template>
+    <div class="addList">
+        <div style="margin: 10px;"></div>
+        <el-form :label-position="labelPosition" label-width="100px" :model="params">
+        <el-form-item label="日付">
+            <el-input placeholder="date" v-model="params.date"></el-input><br>
+        </el-form-item>
+            <el-form-item label="費目">
+                <el-input placeholder="category" v-model="params.category"></el-input><br>
+            </el-form-item>
+            <el-form-item label="買ったもの">
+                <el-input placeholder="item" v-model="params.item"></el-input><br>
+            </el-form-item>
+            <el-form-item label="値段">
+
+                <el-input placeholder="price" v-model="params.price"></el-input><br>
+            </el-form-item>
+            <el-form-item label="メモ">
+                <el-input placeholder="discription" v-model="params.discription"></el-input>
+            </el-form-item>
+        </el-form>
+        <el-button type="primary" icon="el-icon-circle-plus-outline" round @click="addDB"> 追加</el-button>
+    </div>
+</template>
+
+<script>
+    import axios from 'axios'
+    export default {
+        name: "AddList",
+        data() {
+            return {
+                labelPosition: 'right',
+                params: {
+                    date: '',
+                    category: '',
+                    item: '',
+                    price: '',
+                    discription: ''
+                }
+            }
+        },
+        methods: {
+            addDB() {
+                let usp = new URLSearchParams();
+
+                usp.append('params', this.params);
+                axios.post('http://localhost:4567/post', usp)
+                    .then(
+                        response => {
+                            alert('test : ' + response.data)
+                        }
+                    )
+
+            //     axios.post('http://localhost:4567/post', {
+            //         date: this.params.date,
+            //         category: this.params.category,
+            //         item: this.params.item,
+            //
+                //  price: this.params.price,
+            //         discription: this.params.discription
+            // })
+
+            }
+        }
+
+    }
+</script>
+
+<style scoped>
+.el-input {
+    width: 400px;
+}
+</style>
